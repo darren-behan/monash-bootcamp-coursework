e8aab53793ef6bab4adbcdadb628b6a9fa6d2a3e
Darren Behan <darrenbehan@hotmail.com>
2020-07-01 18:40:25 +1000

    week-8-activities

---
+++ pre-class-activities/script_wk_eight.js
@@ -0,0 +1,6 @@
+// Write a function that takes in a number and returns the corresponding day of the week
+
+function result(x) {
+  var days = [undefined, "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  return days[x];
+}
